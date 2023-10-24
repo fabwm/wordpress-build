ef201f7d94fef6fbd6b597a0398a80410e033dd3
fabwm <106402940+fabwm@users.noreply.github.com>
2023-10-24 10:05:20 -0300

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -11,17 +11,3 @@ resource "aws_instance" "wordpress" {
     Name = "wordpress"
   }
 }
-
-# resource "aws_instance" "test" {
-#   ami             = "ami-026ebd4cfe2c043b2"
-#   instance_type   = "t2.micro"
-#   key_name        = aws_key_pair.terraform-key.key_name
-#   user_data       = <<-EOF
-#                 #!/bin/bash
-#                 sudo yum update -y
-#                 EOF
-#   security_groups = ["labs"]
-#   tags = {
-#     Name = "test"
-#   }
-# }
